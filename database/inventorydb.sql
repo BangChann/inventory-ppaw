e6e7949cf46e9fca13f855db31ef9484e0fbfda6
jaleisme <jaleisme.id@gmail.com>
2022-11-04 10:52:26 +0700

    Dashboard Done

--- database/inventorydb.sql
+++ database/inventorydb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 02, 2022 at 05:44 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.22
+-- Generation Time: Nov 04, 2022 at 04:51 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,42 +38,57 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `username`, `password`) VALUES
-(1, 'admin', '$2a$12$Em6TmmMUxLcr5t6JPrE0SejswCG2ixgkLKNdT5Y1jRAitMHwk4/P6');
+(3, 'admin', '$2a$10$9fDh7zUvKyGoUq82kTUrGO32a5dmOcv1Kq7b9NhyEIBuEF2qacqgG'),
+(4, 'admin111', '$2a$10$IQOmUeacPupPVc1w/zm3weCLMVAN4BQX.Z5IqZ8qPDQXI5m/eSsoy');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Table structure for table `categories`
 --
 
-CREATE TABLE `customers` (
+CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Reusable');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `items`
 --
 
-CREATE TABLE `orders` (
+CREATE TABLE `items` (
   `id` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `pid` int(11) NOT NULL,
-  `uid` int(11) NOT NULL,
-  `qty` int(11) NOT NULL,
-  `status` varchar(100) NOT NULL DEFAULT 'ordered'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `shelf_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `qty` int(11) NOT NULL DEFAULT 1,
+  `img` varchar(50) NOT NULL,
+  `status` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `shelf_id`, `name`, `description`, `qty`, `img`, `status`) VALUES
+(4, 1, 'Mini Projector', 'Yellow Colored', 1, 'e8ae398a248c5dabf26af8049c02bb45.jpg', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Table structure for table `items_old`
 --
 
-CREATE TABLE `products` (
+CREATE TABLE `items_old` (
   `id` int(11) NOT NULL,
   `name` varchar(1000) NOT NULL,
   `descr` text NOT NULL,
@@ -81,6 +96,53 @@ CREATE TABLE `products` (
   `stock` int(11) NOT NULL,
   `picture` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items_old`
+--
+
+INSERT INTO `items_old` (`id`, `name`, `descr`, `price`, `stock`, `picture`) VALUES
+(11, 'apa', 'an tuh', 100, 100, '371c847f3f52de43a9a6b914c7284026.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelves`
+--
+
+CREATE TABLE `shelves` (
+  `id` int(11) NOT NULL,
+  `shelf_name` varchar(50) NOT NULL,
+  `location` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shelves`
+--
+
+INSERT INTO `shelves` (`id`, `shelf_name`, `location`, `description`) VALUES
+(1, 'A-1', 'Room A', '4 Level Grey Rack'),
+(5, 'A-2', 'Room A', '5 Level Stories Cabinets');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(8, 'Elektronik'),
+(9, 'ATK');
 
 --
 -- Indexes for dumped tables
@@ -93,21 +155,34 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `customers`
+-- Indexes for table `categories`
 --
-ALTER TABLE `customers`
+ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `orders`
+-- Indexes for table `items`
 --
-ALTER TABLE `orders`
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `shelf_id` (`shelf_id`);
+
+--
+-- Indexes for table `items_old`
+--
+ALTER TABLE `items_old`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `products`
+-- Indexes for table `shelves`
 --
-ALTER TABLE `products`
+ALTER TABLE `shelves`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `types`
+--
+ALTER TABLE `types`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -118,25 +193,47 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT for table `items`
 --
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `items_old`
 --
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `items_old`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT for table `shelves`
 --
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `shelves`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `FK_items_shelves` FOREIGN KEY (`shelf_id`) REFERENCES `shelves` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
